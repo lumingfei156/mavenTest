41c908d6f998d70e0ef41845531de46dfee71f18
李舜生 <33613049+lumingfei156@users.noreply.github.com>
2018-11-20 10:17:30 +0800

    测试代码提交。。。。。 提交中。。。。。

---
+++ src/main/java/com/maven/test1/Test18.java
@@ -0,0 +1,23 @@
+package com.maven.test1;
+
+
+import com.maven.javaBean.LinkQueue;
+import com.maven.javaBean.Service.ImyQueue;
+
+public class Test18 {
+
+    public static void main(String[] args) {
+        ImyQueue<String> linkQueue = new LinkQueue<>();
+        linkQueue.add("1");
+        linkQueue.add("2");
+        linkQueue.add("3");
+        linkQueue.add("4");
+
+        linkQueue.poll();
+        linkQueue.poll();
+        linkQueue.add("5");
+
+        System.out.println(linkQueue);
+
+    }
+}
