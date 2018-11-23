0402f21599cfefc2c7be1d676740c2d78a53cc09
李舜生 <33613049+lumingfei156@users.noreply.github.com>
2018-11-23 16:08:03 +0800

    开哥讲课测试文件，开哥牛逼，开哥无敌

---
+++ src/main/java/com/maven/test1/Test20.java
@@ -0,0 +1,25 @@
+package com.maven.test1;
+
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
+
+/**
+ * @author lulu
+ */
+public class Test20 {
+
+    public static void main(String[] args) {
+//        System.out.println("3.31 " +  new BigDecimal("3.31",new MathContext(2, RoundingMode.UP)));
+//        System.out.println("-3.31 " +  new BigDecimal("-3.31",new MathContext(2, RoundingMode.UP)));
+//        System.out.println("3.31 " + new BigDecimal("3.31",new MathContext(1,RoundingMode.DOWN)));
+//        System.out.println("3.31 " + new BigDecimal("3.31",new MathContext(1,RoundingMode.CEILING)));
+//        System.out.println("3.31 " + new BigDecimal("3.31",new MathContext(1,RoundingMode.FLOOR)));
+//        System.out.println(Integer.MAX_VALUE + 1);
+//        System.out.println(Integer.MAX_VALUE);
+//        Byte t = new Byte((byte)1);
+//        System.out.println(Short.reverseBytes(s));
+//        System.out.println(Integer.MAX_VALUE);
+//        System.out.println(Integer.MIN_VALUE);
+    }
+}
