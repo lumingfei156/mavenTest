c9eb9a89dad482d60dff3b911bf13062c5b2b082
李舜生 <33613049+lumingfei156@users.noreply.github.com>
2018-11-26 17:54:32 +0800

    测试类是有序查找测试类

--- src/main/java/com/maven/test1/Test21.java
+++ src/main/java/com/maven/test1/Test21.java
@@ -1,9 +1,9 @@
 package com.maven.test1;
 
-import com.maven.javaBean.SequentialSearch;
+import com.maven.javaBean.Search;
 
 /**
- * 用于测试顺序查找
+ * 用于测试有序查找
  * @author lulu
  */
 public class Test21 {
@@ -11,9 +11,10 @@ public class Test21 {
     public static void main(String[] args) {
         Integer[] array = new Integer[]{1,2,3,4,5,6,7,8,9};
         int key = 9;
-        SequentialSearch<Integer> search = new SequentialSearch<>(array,key);
+        Search<Integer> search = new Search<>(array,key);
         System.out.println( search.sequentialSearch());
         System.out.println(search.sentinelSearch());
+        System.out.println(search.halfSearch());
+        System.out.println(search.insertSearch());
     }
-
 }
