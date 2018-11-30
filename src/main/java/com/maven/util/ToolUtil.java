7a42d47e68d0deed366c8374d9e7d8324e44010f
李舜生 <33613049+lumingfei156@users.noreply.github.com>
2018-11-30 17:56:58 +0800

    添加了一个空方法

--- src/main/java/com/maven/util/ToolUtil.java
+++ src/main/java/com/maven/util/ToolUtil.java
@@ -1,9 +1,18 @@
 package com.maven.util;
 
+import java.util.Map;
+
 /**
  * @author lulu
  * @since 2018/11/29
  */
 public class ToolUtil {
-
+    /**
+     * 输入sql查询语句，获取，查询语句中列名称与别名的对应关系
+     * @param sql
+     * @return
+     */
+    public static Map<String,String> getSqlName (String sql){
+        return null;
+    }
 }
